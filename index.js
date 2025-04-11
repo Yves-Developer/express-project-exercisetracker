918d7735a20c9e1554bab3d9928261d213ac544f
Yves DC <97420310+Yves-Developer@users.noreply.github.com>
2025-04-11 14:23:35 +0000

    # Import all necessary package

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express()
-const cors = require('cors')
 require('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: false }));
